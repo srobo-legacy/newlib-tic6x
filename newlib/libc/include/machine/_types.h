653924bef17dc6e5098a5c36eac735436232b7e6
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-07-01 18:09:43 +0100

    Define wint_t for TIC64X in machine headers; normally comes with gcc.

--- newlib/libc/include/machine/_types.h
+++ newlib/libc/include/machine/_types.h
@@ -5,4 +5,14 @@
 #ifndef _MACHINE__TYPES_H
 #define _MACHINE__TYPES_H
 #include <machine/_default_types.h>
+
+/*
+ * TIC64X: Because we're not always using GCC, the wint_t type is not always
+ * defined. It's also used throughout newlib; so, define it here as a
+ * workaround
+ */
+#if defined(__TIC64X__)
+typedef unsigned int wint_t;
+#endif
+
 #endif
